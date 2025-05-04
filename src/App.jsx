6b4b0153caed97d0b922cf7daaefd985510e98a6
AbhiDwivedi <abhidwivedi687@gmail.com>
2025-05-05 00:35:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+function App() {
+  return (
+    <div className="text-red-500 text-2xl font-bold">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
+
